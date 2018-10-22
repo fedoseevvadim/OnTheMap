b7f036675ce8aa0234366a516f019f0f54c38e5d
Вадим Федосеев <vadim@uk-pers-07.int.bit.ru>
2018-10-22 10:34:27 +0300

    Initial Commit

---
+++ On_the_map/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  On_the_map
+//
+//  Created by Vadim on 22/10/2018.
+//  Copyright © 2018 Vadim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
